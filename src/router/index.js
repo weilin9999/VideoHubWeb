1f792672ffb4276443e1f78476cd310ab23bb98f
weilin9999 <w@wltip.com>
2022-06-18 03:38:45 +0800

    初始化文件创建 - Initialization file creation

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 const login = () => import ('../views/login/login.vue');
 
 const routes = [
-	{ path: '/', name: 'login', component: login }
+	{ path: '/', name: 'login_index', component: login },
+	{ path: '/login', name: 'login', component: login }
 ];
 
 export default createRouter({
