48245e044cb7af40070bcda4ea51af66896a06e2
weilin9999 <w@wltip.com>
2022-07-01 13:13:51 +0800

    修改文件 - Change file

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <script>
+import cookies from 'vue-cookies'
 export default {
 	name: 'App',
 	data() {
@@ -16,11 +17,17 @@ export default {
 			
 		}
 	},
-	created() {
+	watch: {
 		
 	},
+	created() {
+		const origin = cookies.get('origin')
+		if(origin == null){
+			this.$router.push({ path: '/login' })
+		}
+	},
 	methods: {
-		
+
 	}	
 }
 </script>
@@ -33,10 +40,31 @@ export default {
 @import '@/assets/iconfont/iconfont.css';
 	body,html,#app{
 		width: 100%;
-		height: 100%;
+		height: auto;
 		margin: 0;
 		padding: 0;
 		top: 0;
 		left: 0;
+	}
+	::-webkit-scrollbar {
+		width:12px;
+		height: 12px;
+	}
+	
+	::-webkit-scrollbar-track {
+		-webkit-box-shadow: inset006px rgba(0,0,0,0.5);
+		border-radius:3px;
+	}
+	
+	::-webkit-scrollbar-thumb {
+		background:#999;
+		border-radius: 7px;
+		outline: 2px solid #fff;
+		outline-offset: -2px;
+		border: 2px solid #fff;
+		-webkit-box-shadow: inset006px rgba(0,0,0,0.5);
+	}
+	::-webkit-scrollbar-thumb:hover{
+		background:#b8b8b8;
 	}
 </style>
