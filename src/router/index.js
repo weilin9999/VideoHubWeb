c9951e3ab4c1d8cfcde599105778e079c162b931
weilin9999 <w@wltip.com>
2022-07-01 13:11:05 +0800

    更改路由 - Change router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
+const mainview = () => import ('../views/mainview/mainview.vue');
 const login = () => import ('../views/login/login.vue');
+const video = () => import ('../views/video/video.vue');
 
 const routes = [
-	{ path: '/', name: 'login_index', component: login },
-	{ path: '/login', name: 'login', component: login }
+	{ path: '/', name: 'mainview', component: mainview },
+	{ path: '/login', name: 'login', component: login },
+	{ path: '/register', name: 'register', component: login },
+	{ path: '/video', name: 'video', component: video },
+	{ path: '/:pathMatch(.*)' , name: '404', redirect: '/' },
 ];
 
 export default createRouter({
