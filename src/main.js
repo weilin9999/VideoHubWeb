5747262938d885c8b85cb3c0ad2368cd03156483
weilin9999 <w@wltip.com>
2022-07-01 13:14:34 +0800

    修改文件 - Change file

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import { createPinia } from 'pinia'
-import VueCookies from 'vue-cookies'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import VueCookies from 'vue-cookies';
+import AlertMsg from '@/components/AlertMsg/AlertMsg.vue';
+import VHSelect from '@/components/VH-Select/VH-Select.vue';
+import VHPage from '@/components/VH-Page/VH-Page.vue';
+import Headers from '@/views/header/header.vue';
+import VHDialog from '@/components/VH-Dialog/VH-Dialog.vue';
 
 const app = createApp(App);
 app.use(router);
 app.use(createPinia);
 app.use(VueCookies);
+app.component("AlertMsg",AlertMsg)
+app.component("VHSelect",VHSelect)
+app.component("VHPage",VHPage)
+app.component("Headers",Headers)
+app.component("VHDialog",VHDialog)
 app.mount('#app');
